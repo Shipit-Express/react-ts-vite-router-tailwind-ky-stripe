8d2680fda986ebc432ab92c8aac4dc8a4214b96a
Labham Jain <labhamjain81@gmail.com>
2025-03-20 00:12:44 +0530

    fix: remove stripe test key | add env variable

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,7 +2,7 @@ import PaymentForm from "@/components/Payment"
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe('pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3');
+const stripePromise = loadStripe(import.meta.env.STRIPE_PUBLIC_KEY);
 
 
 const CheckoutPage = () => {
